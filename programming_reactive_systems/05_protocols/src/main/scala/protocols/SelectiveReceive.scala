674dd6ea41b2df3f94dae9121e41b69f24631931
Aleksei Litkovetc <turgor_80@mail.ru>
2019-04-08 02:54:05 +0300

    Step 1 of 2: Transactor - SelectiveReceive implementation

--- programming_reactive_systems/05_protocols/src/main/scala/protocols/SelectiveReceive.scala
+++ programming_reactive_systems/05_protocols/src/main/scala/protocols/SelectiveReceive.scala
@@ -1,6 +1,7 @@
 package protocols
 
-import akka.actor.typed._
+import akka.actor.typed.Behavior._
+import akka.actor.typed.{ActorContext, ExtensibleBehavior, _}
 import akka.actor.typed.scaladsl._
 
 object SelectiveReceive {
@@ -16,5 +17,63 @@ object SelectiveReceive {
       * Hint: Implement an [[ExtensibleBehavior]], use a [[StashBuffer]] and [[Behavior]] helpers such as `start`,
       * `validateAsInitial`, `interpretMessage`,`canonicalize` and `isUnhandled`.
       */
-    def apply[T](bufferSize: Int, initialBehavior: Behavior[T]): Behavior[T] = ???
+    def apply[T](bufferSize: Int, initialBehavior: Behavior[T]): Behavior[T] = {
+        val buffer: StashBuffer[T] = StashBuffer(bufferSize)
+
+        implicit class BufferOpts(buffer: StashBuffer[T]) {
+            def unstash(ctx: ActorContext[T], behavior: Behavior[T]): Behavior[T] = {
+                buffer.unstash(ctx.asScala, behavior, numberOfMessages = 1, m => m)
+            }
+        }
+
+        def unstashing(behavior: Behavior[T], size: Int, wasApplied: Boolean = false): Behavior[T] =
+            Behaviors.receive { (ctx: ActorContext[T], msg: T) =>
+                if (wasApplied) {
+                    buffer.stash(msg)
+                    if (size <= 1) {
+                        buffer.unstash(ctx.asScala, unstashing(behavior, buffer.size))
+                    } else {
+                        buffer.unstash(ctx.asScala, unstashing(behavior, size - 1, wasApplied))
+                    }
+                } else {
+                    val next: Behavior[T] = interpretMessage(behavior, ctx, msg)
+                    val nextc: Behavior[T] = canonicalize(behavior = next, current = behavior, ctx)
+                    if (nextc == behavior) {
+                        buffer.stash(msg)
+                        if (size <= 1) {
+                            receiving(behavior)
+                        } else {
+                            buffer.unstash(ctx.asScala, unstashing(behavior, size - 1, wasApplied))
+                        }
+                    } else {
+                        if (size <= 1) {
+                            buffer.unstash(ctx.asScala, unstashing(next, buffer.size))
+                        } else {
+                            buffer.unstash(ctx.asScala, unstashing(next, size - 1, wasApplied = true))
+                        }
+                    }
+                }
+            }
+
+        def receiving(behavior: Behavior[T]): Behavior[T] =
+            Behaviors.receive { (ctx: ActorContext[T], msg: T) =>
+                val next: Behavior[T] = interpretMessage(behavior, ctx, msg)
+                val nextc: Behavior[T] = canonicalize(behavior = next, current = behavior, ctx)
+                if (nextc == behavior) {
+                    buffer.stash(msg)
+                    receiving(behavior)
+                } else {
+                    buffer.unstash(ctx.asScala, unstashing(next, buffer.size))
+                }
+            }
+
+        Behaviors.receive { (ctx: ActorContext[T], msg: T) =>
+            buffer.stash(msg)
+            buffer.unstashAll(ctx.asScala, receiving(initialBehavior))
+        }.receiveSignal { case (ctx: ActorContext[T], msg: T) =>
+            Behavior.stopped
+        }
+    }
+
+
 }
